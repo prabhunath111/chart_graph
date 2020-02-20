3e4501e32f7368e45a984d58149a1eba23a82899
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-02-20 19:35:01 +0530

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,341 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    showSemanticsDebugger: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+          primaryColor: Colors.blue
+    ),
+    home: MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+
+    final List<SalesData> chartData = [
+      SalesData(2010, 35),
+      SalesData(2011, 28),
+      SalesData(2012, 34),
+      SalesData(2013, 32),
+      SalesData(2014, 40)
+    ];
+    final List<ChartData> chartDataCircle = [
+      ChartData('David', 25),
+      ChartData('Steve', 38),
+      ChartData('Jack', 34),
+      ChartData('Others', 52)
+    ];
+
+    final List<ChartDataLebel> chartDataLebel = [
+      ChartDataLebel('2010', 35, 0.32),
+      ChartDataLebel('2011', 38, 0.21),
+      ChartDataLebel('2012', 34, 0.38),
+      ChartDataLebel('2014', 40, 0.34)
+    ];
+    final List<SalesDataBar> chartDataBar = [
+      SalesDataBar(2010,  23),
+      SalesDataBar(2011, 49),
+      SalesDataBar(2012,  12),
+      SalesDataBar(2013,  33),
+      SalesDataBar(2014,  30)
+    ];
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+          appBar: AppBar(
+            leading: Icon(Icons.close),
+            backgroundColor: Colors.black,
+            title: Text('Live Stream Report',style: TextStyle(color: Colors.white),),
+            actions: <Widget>[
+              PopupMenuButton<String>(
+                itemBuilder: (BuildContext context){
+                  return ;
+                },
+              )
+            ],
+          ),
+          body:
+          /*Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: (MediaQuery.of(context).size.width)*0.5,
+                          height: (MediaQuery.of(context).size.height)*0.4,
+                          color: Colors.grey,
+                          child: SfCartesianChart(
+                              title: ChartTitle(
+                                text: 'Area Chart',
+                                alignment: ChartAlignment.near,
+                              ),
+                              series: <ChartSeries>[
+                                // Renders area chart
+                                AreaSeries<SalesData, double>(
+                                    dataSource: chartData,
+                                    xValueMapper: (SalesData sales, _) => sales.year,
+                                    yValueMapper: (SalesData sales, _) => sales.sales
+                                )
+                              ]
+                          )
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                          width: (MediaQuery.of(context).size.width)*0.5,
+                          height: (MediaQuery.of(context).size.height)*0.4,
+                          color: Colors.grey,
+                          child: SfCartesianChart(
+                              title: ChartTitle(
+                                text: 'Line Chart',
+                                alignment: ChartAlignment.near,
+                              ),
+                              series: <ChartSeries>[
+                                // Renders fast line chart
+                                FastLineSeries<SalesData, double>(
+                                    dataSource: chartData,
+                                    xValueMapper: (SalesData sales, _) => sales.year,
+                                    yValueMapper: (SalesData sales, _) => sales.sales
+                                )
+                              ]
+                          )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                          width: (MediaQuery.of(context).size.width)*0.7,
+                          height: (MediaQuery.of(context).size.height)*0.4,
+                          color: Colors.grey,
+                          child: SfCircularChart(
+                              title: ChartTitle(
+                                text: 'pie Chart',
+                                alignment: ChartAlignment.near,
+                              ),
+                              series: <CircularSeries>[
+                                // Render pie chart
+                                PieSeries<ChartData, String>(
+                                    dataSource: chartDataCircle,
+                                    pointColorMapper:(ChartData data,  _) => data.color,
+                                    xValueMapper: (ChartData data, _) => data.x,
+                                    yValueMapper: (ChartData data, _) => data.y
+                                )
+                              ]
+                          )
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child:  Container(
+                          height: (MediaQuery.of(context).size.height)*0.4,
+
+                          color: Colors.grey,
+                          child: SfCartesianChart(
+                              primaryXAxis: CategoryAxis(),
+                              title: ChartTitle(
+                                text: 'Data lebel Chart',
+                                alignment: ChartAlignment.near,
+                              ),
+                              series: <ChartSeries>[
+                                RangeColumnSeries<ChartDataLebel, String>(
+                                  dataSource: chartDataLebel,
+                                  xValueMapper: (ChartDataLebel sales, _) => sales.x,
+                                  lowValueMapper: (ChartDataLebel sales, _) => sales.low,
+                                  highValueMapper: (ChartDataLebel sales, _) => sales.high,
+                                  dataLabelSettings: DataLabelSettings(
+                                    isVisible: true,
+//                              position: CartesianLabelPosition.top
+                                  ),
+                                )
+                              ]
+                          )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )*/
+          SafeArea(
+            child: OrientationBuilder(builder: (context, orientation) {
+              return GridView.count(
+                crossAxisCount: orientation == Orientation.portrait ? 2 : 2,
+                crossAxisSpacing: 2.0,
+                mainAxisSpacing: 2.0,
+                children: <Widget>[
+                  Container(
+                    color: Colors.grey,
+                      child: SfCartesianChart(
+                        title: ChartTitle(
+                          text: 'Area Chart',
+                          alignment: ChartAlignment.near,
+                        ),
+                          series: <ChartSeries>[
+                            // Renders area chart
+                            AreaSeries<SalesData, double>(
+                                dataSource: chartData,
+                                xValueMapper: (SalesData sales, _) => sales.year,
+                                yValueMapper: (SalesData sales, _) => sales.sales
+                            )
+                          ]
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      child: SfCartesianChart(
+                          title: ChartTitle(
+                            text: 'Line Chart',
+                            alignment: ChartAlignment.near,
+                          ),
+                          series: <ChartSeries>[
+                            // Renders fast line chart
+                            FastLineSeries<SalesData, double>(
+                                dataSource: chartData,
+                                xValueMapper: (SalesData sales, _) => sales.year,
+                                yValueMapper: (SalesData sales, _) => sales.sales
+                            )
+                          ]
+                      )
+                  ),
+
+                       Container(
+                            color: Colors.grey,
+                            child: SfCircularChart(
+                                title: ChartTitle(
+                                  text: 'pie Chart',
+                                  alignment: ChartAlignment.near,
+                                ),
+                                series: <CircularSeries>[
+                              // Render pie chart
+                              PieSeries<ChartData, String>(
+                                  dataSource: chartDataCircle,
+                                  pointColorMapper:(ChartData data,  _) => data.color,
+                                  xValueMapper: (ChartData data, _) => data.x,
+                                  yValueMapper: (ChartData data, _) => data.y
+                              )
+                            ]
+                            )
+                        ),
+
+
+                  Container(
+                      color: Colors.grey,
+                      child: SfCartesianChart(
+                          primaryXAxis: CategoryAxis(),
+                          title: ChartTitle(
+                            text: 'Data lebel Chart',
+                            alignment: ChartAlignment.near,
+                          ),
+                          series: <ChartSeries>[
+                            RangeColumnSeries<ChartDataLebel, String>(
+                              dataSource: chartDataLebel,
+                              xValueMapper: (ChartDataLebel sales, _) => sales.x,
+                              lowValueMapper: (ChartDataLebel sales, _) => sales.low,
+                              highValueMapper: (ChartDataLebel sales, _) => sales.high,
+                              dataLabelSettings: DataLabelSettings(
+                                  isVisible: true,
+//                              position: CartesianLabelPosition.top
+                              ),
+                            )
+                          ]
+                      )
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      child: SfCartesianChart(
+                          title: ChartTitle(
+                            text: 'Bar Chart',
+                            alignment: ChartAlignment.near,
+                          ),
+                          series: <ChartSeries>[
+                            // Renders bar chart
+                            BarSeries<SalesDataBar, double>(
+                                dataSource: chartDataBar,
+                                xValueMapper: (SalesDataBar sales, _) => sales.year,
+                                yValueMapper: (SalesDataBar sales, _) => sales.sales
+                            )
+                          ]
+                      ),
+                  ),
+                  Container(
+                      color: Colors.grey,
+                      child: SfCartesianChart(
+                          title: ChartTitle(
+                            text: 'Bar Chart',
+                            alignment: ChartAlignment.near,
+                          ),
+                          series: <ChartSeries>[
+                            // Renders bar chart
+                            BarSeries<SalesDataBar, double>(
+                                dataSource: chartDataBar,
+                                xValueMapper: (SalesDataBar sales, _) => sales.year,
+                                yValueMapper: (SalesDataBar sales, _) => sales.sales
+                            )
+                          ]
+                      )
+                  ),
+
+                ],
+              );
+            }),
+          )
+
+
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
+
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class SalesData {
+  SalesData(this.year, this.sales);
+  final double year;
+  final double sales;
+}
+class ChartData {
+  ChartData(this.x, this.y, [this.color]);
+  final String x;
+  final double y;
+  final Color color;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ChartDataLebel {
+  ChartDataLebel(this.x, this.high, this.low);
+  final String x;
+  final double high;
+  final double low;
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+class SalesDataBar {
+  SalesDataBar(this.year, this.sales);
+  final double year;
+  final double sales;
 }
